0fcff38ff118deadfbeb450bb3276db5170e0e25
TWChromeTW <orlov707kostya@gmail.com>
2026-03-21 15:32:15 +0300

    refactor: do todo
    split __init__
    add all widgets (not main) to self
    
    bonus: add load settings size of h_splitter and write small comments to code

--- gui/main.py
+++ gui/main.py
@@ -16,68 +16,64 @@ from PyQt6.QtCore import (
 from PyQt6.QtGui import QAction
 
 class MainWindow(QMainWindow):
-    """
-    todo:
-        1) change constructor (__init__), bcs i think it`s very overload
-            so many code/line in one method
-        2) maybe add all widget to self, bcs it`s should be like fields in classic OOP (C++/Java)
-    """
-    def __init__(self): 
+    def __init__(self):
+        # parent's constructor (QMainWindow)
         super().__init__()
 
-        self.settings = QSettings("Degustation", "IFCEditor")
-
-        """python`s trash
-        geometry = self.settings.value("geometry")
-        if geometry: self.restoreGeometry(geometry)
-
-        v_state = self.settings.value("v_splitter_state")
-        if v_state: self.v_splitter.restoreState(v_state)
-        """
-
+        # title and default suze
         self.setWindowTitle("IFC editor")
         self.resize(800, 600)
 
-        #main widgets
+        # initialization settings for load settings after last close
+        self.settings = QSettings("Degustation", "IFCEditor")
+
+        # build main interface
+        self.__init_ui()
+        self.__create_menu()
+
+        # load settings (AFTER BUILD ALL WIDGETS)
+        self.__restore_settings()
+
+
+    def __init_ui(self):
+        # two main widget
         main_widget = QWidget()
         main_layout = QVBoxLayout()
-        
-        #splitters
+
+        # create another widget
+
         self.v_splitter = QSplitter(Qt.Orientation.Vertical)
-        self.v_splitter.setSizes([500, 100])
-        self.h_splitter = QSplitter(Qt.Orientation.Horizontal) 
-        
-        geometry = self.settings.value("geometry")
-        if geometry: self.restoreGeometry(geometry)
+        self.h_splitter = QSplitter(Qt.Orientation.Horizontal)
 
-        v_state = self.settings.value("v_splitter_state")
-        if v_state: self.v_splitter.restoreState(v_state)
+        # tree, bottom_panel and viewport NOW JUST plugs
+        self.tree = QTreeWidget()
+        self.tree.setHeaderLabel("Struct of IFC")
 
-        #empty widgets (empty just now)
-        tree = QTreeWidget()
-        tree.setHeaderLabel("Struct of IFC")
-        
-        viewport = QWidget()
-        viewport.setStyleSheet("background-color: #333333;") #just color
+        self.viewport = QWidget()
+        self.viewport.setStyle("background-color: #333333;")
 
-        bottom_panel = QTextEdit()
-        bottom_panel.setPlaceholderText("Place for logs")
+        self.bottom_panel = QTextEdit()
+        self.bottom_panel.setPlaceholderText("Place for logs")
 
-        self.h_splitter.addWidget(tree)
-        self.h_splitter.addWidget(viewport)
+        # add plugs to splitter
+        self.h_splitter.addWidget(self.tree)
+        self.h_splitter.addWidget(self.viewport)
 
         self.v_splitter.addWidget(self.h_splitter)
-        self.v_splitter.addWidget(bottom_panel)
+        self.v_splitter.addWidget(self.bottom_panel)
 
+        # set default size on first open
+        self.v_splitter.setSizes([500, 100])
 
+        # add all to main widgets
         main_layout.addWidget(self.v_splitter)
-
         main_widget.setLayout(main_layout)
 
+        # add main widget to MainWindow
         self.setCentralWidget(main_widget)
-        self.statusBar().showMessage("Ready to work")
 
-        self.__create_menu()
+        # just status bar
+        self.statusBar().showMessage("Ready to work")
 
     def __create_menu(self):
         menu_bar = self.menuBar()
@@ -88,9 +84,25 @@ class MainWindow(QMainWindow):
 
         file_menu.addAction(exit_action)
 
+    def __restore_settings(self):
+        """mehtod for save size window and splitters"""
+        geometry = self.settings.value("geometry")
+        if geometry:
+            self.restoreGeometry(geometry)
+        
+        v_state = self.settings.value("v_splitter_state")
+        if v_state:
+            self.v_splitter.restoreState(v_state)
+
+        h_state = self.settings.value("h_splitter_state")
+        if h_state:
+            self.h_splitter.restoreState(h_state)
+
     def closeEvent(self, event):
+        """this method called before close app"""
         self.settings.setValue("geometry", self.saveGeometry())
         self.settings.setValue("v_splitter_state", self.v_splitter.saveState())
+        self.settings.setValue("h_splitter_state", self.h_splitter.saveState())
 
         super().closeEvent(event)
 
